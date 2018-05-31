a6d7e10c00e1d6b333bddbae7cb01f83490fcc52
DanielD <danidiaza@gmail.com>
2018-05-30 18:52:29 -0600

    Dibujar por lineas, agregar lineas blancas relativas al horizonte

--- Display.cpp
+++ Display.cpp
@@ -9,34 +9,129 @@
 
 
 
-Display::Display(uint16_t i_Posit)
+Display::Display( )
 {
     // - Esto no hace nada por el momento
-    m_u16BITN = i_Posit;
-    m_i16HorizonHeight = 128/2;
+
+
+    m_u16DrawIter = 0;
+    m_i16HorizonHeight = -1;
+    m_i8DrawDir = DD_DOWN;
 }
 
 uint8_t Display::run()
 {
     GetHorizon();
 
-    Graphics_Rectangle Rect1 = {0, 0, 128, m_i16HorizonHeight}; // - Rectangle 1 dimensions
-    Graphics_Rectangle Rect2 = {0, m_i16HorizonHeight, 128, 128}; // - Rectangle 2 dimensions
+    if (m_u16DrawIter == 0){
+        if (m_i16NextHorizon > m_i16HorizonHeight){
+            m_i8DrawDir = DD_DOWN;
+            Graphics_setForegroundColor(&m_stContext, GRAPHICS_COLOR_ROYAL_BLUE);
+        } else {
+            m_i8DrawDir = DD_UP;
+            Graphics_setForegroundColor(&m_stContext, GRAPHICS_COLOR_SIENNA);
+        }
+        m_i16HorizonHeight = m_i16NextHorizon;
+    }
+    if (m_i8DrawDir == DD_DOWN){
+        DrawSectionDown();
+    }
+    else {
+        DrawSectionUp();
+    }
 
-    Graphics_setForegroundColor(&m_stContext, GRAPHICS_COLOR_ROYAL_BLUE);
-    Graphics_fillRectangle(&m_stContext, &Rect1);
+    //Graphics_Rectangle Rect1 = {0, 0, 128, m_i16HorizonHeight}; // - Rectangle 1 dimensions
+    //Graphics_Rectangle Rect2 = {0, m_i16HorizonHeight, 128, 128}; // - Rectangle 2 dimensions
+
+    //Graphics_setForegroundColor(&m_stContext, GRAPHICS_COLOR_ROYAL_BLUE);
+    //Graphics_fillRectangle(&m_stContext, &Rect1);
 
     // Test code to draw a rectangles
-    Graphics_setForegroundColor(&m_stContext, GRAPHICS_COLOR_SIENNA);
-    Graphics_fillRectangle(&m_stContext, &Rect2);
+    //Graphics_setForegroundColor(&m_stContext, GRAPHICS_COLOR_SIENNA);
+    //Graphics_fillRectangle(&m_stContext, &Rect2);
     return(NO_ERR);
+}
+
+void Display::DrawSectionDown()
+{
+    if (m_u16DrawIter == m_i16HorizonHeight){
+        Graphics_setForegroundColor(&m_stContext, GRAPHICS_COLOR_WHITE_SMOKE);
+    }
+    if (m_u16DrawIter == m_i16HorizonHeight+1){
+        Graphics_setForegroundColor(&m_stContext, GRAPHICS_COLOR_SIENNA);
+    }
+
+
+    if (m_u16DrawIter == m_i16HorizonHeight - SMALL_LINE){
+        DrawHalfLine(SMALL_LINE_WIDTH, m_u16DrawIter, GRAPHICS_COLOR_ROYAL_BLUE);
+    } else if (m_u16DrawIter == m_i16HorizonHeight + SMALL_LINE){
+        DrawHalfLine(SMALL_LINE_WIDTH, m_u16DrawIter, GRAPHICS_COLOR_SIENNA);
+
+    } else if (m_u16DrawIter == m_i16HorizonHeight - LONG_LINE){
+        DrawHalfLine(LONG_LINE_WIDTH, m_u16DrawIter, GRAPHICS_COLOR_ROYAL_BLUE);
+    } else if (m_u16DrawIter == m_i16HorizonHeight + LONG_LINE){
+        DrawHalfLine(LONG_LINE_WIDTH, m_u16DrawIter, GRAPHICS_COLOR_SIENNA);
+
+    } else {
+        Graphics_drawLineH(&m_stContext, 0, 128, m_u16DrawIter);
+    }
+
+    m_u16DrawIter++;
+
+    if (m_u16DrawIter >= 128) {
+        m_u16DrawIter = 0;
+        Graphics_setForegroundColor(&m_stContext, GRAPHICS_COLOR_ROYAL_BLUE);
+    }
+    return;
+}
+
+void Display::DrawSectionUp()
+{
+    uint16_t l_u16IterInv = 127-m_u16DrawIter;
+
+    if (l_u16IterInv == m_i16HorizonHeight){
+        Graphics_setForegroundColor(&m_stContext, GRAPHICS_COLOR_WHITE_SMOKE);
+    }
+    if (l_u16IterInv == m_i16HorizonHeight-1){
+        Graphics_setForegroundColor(&m_stContext, GRAPHICS_COLOR_ROYAL_BLUE);
+    }
+
+    if (l_u16IterInv == m_i16HorizonHeight-SMALL_LINE){
+         DrawHalfLine(SMALL_LINE_WIDTH, l_u16IterInv, GRAPHICS_COLOR_ROYAL_BLUE);
+    } else if(l_u16IterInv == m_i16HorizonHeight+SMALL_LINE){
+         DrawHalfLine(SMALL_LINE_WIDTH, l_u16IterInv, GRAPHICS_COLOR_SIENNA);
+
+    } else if (l_u16IterInv == m_i16HorizonHeight-LONG_LINE){
+        DrawHalfLine(LONG_LINE_WIDTH, l_u16IterInv, GRAPHICS_COLOR_ROYAL_BLUE);
+    } else if(l_u16IterInv == m_i16HorizonHeight+LONG_LINE){
+        DrawHalfLine(LONG_LINE_WIDTH, l_u16IterInv, GRAPHICS_COLOR_SIENNA);
+
+    } else {
+        Graphics_drawLineH(&m_stContext, 0, 128, l_u16IterInv);
+    }
+    m_u16DrawIter++;
+
+    if (m_u16DrawIter >= 128) {
+        m_u16DrawIter = 0;
+        Graphics_setForegroundColor(&m_stContext, GRAPHICS_COLOR_SIENNA);
+    }
+    return;
+}
+
+void Display::DrawHalfLine(int16_t i_i16Width, int16_t i_i16Y, int32_t i_i32Color)
+{
+    Graphics_drawLineH(&m_stContext, 0, (128 - i_i16Width)/2, i_i16Y);
+    Graphics_setForegroundColor(&m_stContext, GRAPHICS_COLOR_WHITE_SMOKE);
+    Graphics_drawLineH(&m_stContext, (128 - i_i16Width)/2, (128 + i_i16Width)/2, i_i16Y);
+    Graphics_setForegroundColor(&m_stContext, i_i32Color);
+    Graphics_drawLineH(&m_stContext, (128 + i_i16Width)/2, 128, i_i16Y);
 }
 
 bool Display::GetHorizon()
 {
     st_Message l_stMensaje = getMessage(m_u8TaskID);
     if (l_stMensaje.bMessageValid && l_stMensaje.u8MessageCode == i16ScalarMessage){
-        m_i16HorizonHeight = (int16_t) l_stMensaje.u32MessageData;
+        m_i16NextHorizon = (int16_t) l_stMensaje.u32MessageData;
         return true;
     } else {
         return false;
@@ -64,6 +159,7 @@ uint8_t Display::setup()
     // - Clears Display
     Graphics_clearDisplay(&m_stContext);
 
+    Graphics_setForegroundColor(&m_stContext, GRAPHICS_COLOR_ROYAL_BLUE);
 
 
     return(NO_ERR);
--- Display.hpp
+++ Display.hpp
@@ -19,11 +19,20 @@ extern "C"
     #include "Crystalfontz128x128_ST7735.h"
 }
 
+#define DD_DOWN 0
+#define DD_UP   1
+
+#define SMALL_LINE 25
+#define SMALL_LINE_WIDTH 25
+
+#define LONG_LINE 50
+#define LONG_LINE_WIDTH 75
+
 class Display : public Task
 {
     public:
-        Display(uint16_t);
-        uint16_t m_u16BITN;
+        Display();
+        //uint16_t m_u16BITN;
         virtual uint8_t run(void);
         virtual uint8_t setup(void);
 
@@ -31,7 +40,15 @@ class Display : public Task
     private:
         Graphics_Context m_stContext;
         int16_t m_i16HorizonHeight;
+        int16_t m_i16NextHorizon;
+        int8_t m_i8DrawDir;
         bool GetHorizon();
+
+        uint16_t m_u16DrawIter;
+        void DrawSectionDown();
+        void DrawSectionUp();
+
+        void DrawHalfLine(int16_t i_i16Width, int16_t i_i16Y, int32_t i_i32Color);
 };
 
 #endif /* DISPLAY_HPP_ */
